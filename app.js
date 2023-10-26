9867215476c4c24169c595f8f838071190c02f31
sarita <bsarita926@gmail.com>
2023-10-26 12:59:09 +0545

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let number=prompt("Enter a number");
+switch(number)
+{
+    case 1:case 2:case 11:case 12:
+    
+        document.write("Winter Season");
+    case 3:case 4:case 5:case 6:
+        document.write("Summer season");
+    case 7:case 8:case 9:case 10:
+        document.write("Rainy Season");
+        default:
+            document.write("YOu entered wrong number");
+}
